eefa2b8a57c6e030b3384cd29dc9c07f2114c49c
yomnabarakate21 <yomnabarakate21@gmail.com>
2017-12-25 20:35:15 +0200

    ADD New reader class

--- src/huffman/ReadFile.java
+++ src/huffman/ReadFile.java
@@ -9,42 +9,65 @@ package huffman;
  *
  * @author yomnabarakat
  */
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 public class ReadFile {
 
-   public static void main(String[] args) {      
-      File f = null;
-      String path;
-      long len;
-      boolean bool = false;
-      
-      try { 
-      
-         // create new file
-         f = new File("input.txt");
-         
-         // true if the file path is a file, else false
-         bool = f.exists();
-         
-         // if path exists
-         if(bool) {
-         
-            // returns the length in bytes
-            len = f.length();
-                                 
-            // path
-            path = f.getPath();
-            
-            // print
-            System.out.print(path+" file length: "+len);
-         }
-         
-      } catch(Exception e) {
-         
-         // if any error occurs
-         e.printStackTrace();
-      }
-   }
+    	public static void readFileAndPrintCounts(String crunchifyFile) throws FileNotFoundException {
+ 
+		int crunchifyTotalWords = 0;
+		int crunchifyTotalLines = 0;
+		int crunchifyTotalCharacters = 0;
+ 
+		String crunchifyLine;
+ 
+		// Read file contents
+		// The try-with-resources statement is a try statement that declares one or more resources. A resource is an
+		// object that must be closed after the program is finished with it. The try-with-resources statement ensures
+		// that each resource is closed at the end of the statement.
+		try (BufferedReader crunchifyBuffer = new BufferedReader(new FileReader(crunchifyFile))) {
+			crunchifyLog("========== File Content ==========");
+ 
+			// read each line one by one
+			while ((crunchifyLine = crunchifyBuffer.readLine()) != null) {
+				//crunchifyLog(crunchifyLine);
+				crunchifyTotalLines++;
+ 
+				// ignore multiple white spaces
+				String[] myWords = crunchifyLine.replaceAll("\\s+", " ").split(" ");
+ 
+				for (String s : myWords) {
+					crunchifyTotalCharacters += s.length();
+				}
+ 
+				crunchifyTotalWords += myWords.length;
+ 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		crunchifyLog("\n========== Result ==========");
+ 
+		crunchifyLog("* Total Characters: " + crunchifyTotalCharacters);
+		crunchifyLog("* Total Words: " + crunchifyTotalWords);
+		crunchifyLog("* Toal Lines: " + crunchifyTotalLines);
+	}
+ 
+	private static void crunchifyLog(String string) {
+		System.out.println(string);
+	}
+ 
+	public static void main(String[] args) {
+		try {
+			String crunchifyFile = "input.txt";
+			readFileAndPrintCounts(crunchifyFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-   
+    
+ 
